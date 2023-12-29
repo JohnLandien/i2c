2d56f488ad6e79eb725cf3f29d1482a24c91a041
JohnLandien <john.langhout+github@gmail.com>
2023-12-29 19:57:26 +0100

    wip

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-test
+const i2c = require('i2c-bus');
+const i2c_ADDR1 = 0x20;
+const i2c_ADDR2 = 0x26;
+
+const rbuf = Buffer.alloc(2);
+//const wbuf ;
+
+
+i2c.openPromisified(1).
+then(i2c1 => i2c1.i2cWrite(i2c_ADDR1, 2, 0x0000).
+  then(_ => i2c1.i2cRead(i2c_ADDR1, 2, rbuf)).
+  then(data => console.log(data)).
+  then(_ => i2c1.close())
+).catch(console.log);
