bcc4520f26219f2c67cb584607d5fbe4ba7bec7c
JohnLandien <john.langhout+github@gmail.com>
2023-12-29 20:11:00 +0100

    nkjn

--- test.js
+++ test.js
@@ -8,8 +8,8 @@ const wbuf = Buffer.from([OFF]);
 
 
 i2c.openPromisified(1).
-then(i2c1 => i2c1.i2cWrite(i2c_ADDR1, wbuf.length, wbuf).
-  then(_ => i2c1.i2cRead(i2c_ADDR1, rbuf.length, rbuf)).
+then(i2c1 => i2c1.i2cRead(i2c_ADDR1, rbuf.length, rbuf).
+  then(_ => i2c1.i2cWrite(i2c_ADDR1, wbuf.length, wbuf)).
   then(data => console.log(data)).
   then(_ => i2c1.close())
 ).catch(console.log);
