e7c4025b001a9a4d51df73a4a217e2c167d39d3b
JohnLandien <john.langhout+github@gmail.com>
2023-12-29 20:07:40 +0100

    ww

--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const i2c_ADDR1 = 0x20;
 const i2c_ADDR2 = 0x26;
 const OFF = 0x0000;
 
-const rbuf = Buffer.alloc(2);
+const rbuf = Buffer.alloc(4);
 const wbuf = Buffer.from([OFF]);
 
 
 i2c.openPromisified(1).
-then(i2c1 => i2c1.i2cWrite(i2c_ADDR1, 2, 0x0000).
-  then(_ => i2c1.i2cRead(i2c_ADDR1, 2, rbuf)).
+then(i2c1 => i2c1.i2cWrite(i2c_ADDR1, wbuf.length, wbuf).
+  then(_ => i2c1.i2cRead(i2c_ADDR1, rbuf.length, rbuf)).
   then(data => console.log(data)).
   then(_ => i2c1.close())
 ).catch(console.log);
