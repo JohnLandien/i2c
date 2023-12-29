b84432221faea40fc8e00330e0f39023d221f0fd
JohnLandien <john.langhout+github@gmail.com>
2023-12-29 20:47:56 +0100

    jknkjn

--- testout.js
+++ testout.js
@@ -5,7 +5,7 @@ const OFF = 0x0000;
 const ON = 0xFFFF;
 
 //const rbuf = Buffer.alloc(2);
-//const wbuf = Buffer.from([OFF]);
+const wbuf = Buffer.from([ON]);
 
 
 // i2c.openPromisified(1).
@@ -18,7 +18,7 @@ const ON = 0xFFFF;
 const i2c1 = i2c.open(1, err => {
     if (err) throw err;
   
-    i2c1.writeI2cBlock(i2c_ADDR1, 0, 4,  ON, (err, rawData) => {
+    i2c1.writeI2cBlock(i2c_ADDR1, 0, wbuf.length, wbuf, (err, rawData) => {
       if (err) throw err;
   
       console.log(rawData);
